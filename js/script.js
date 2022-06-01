5acbd8ccd95d5b9df7c8a5ac04f7e08dd7307475
Ankush kulshrestha <85397813+Ankush9927@users.noreply.github.com>
2022-06-01 23:49:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-bar');
+let menu = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+	menu.classList.toggle('fa-bars');
+	navbar.classList.toggle('abc');
+}
+
+
+
+
+
